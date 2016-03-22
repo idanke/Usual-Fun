2c5ab157dcf82fb2cd333c519b9ae2d977f14f2a
Allen <idanke.zm@gmail.com>
2016-03-22 15:27:47 +0800

    增加注释

--- DisabledTimer.js
+++ DisabledTimer.js
@@ -1,11 +1,15 @@
+//按钮禁用倒计时
 function DisabledTimer(dom){
 	if($(dom).data('timer') != undefined){
-		var timerNum = $(dom).data('timer');
+		var timerNum = $(dom).data('timer');	//读取预设值
 	}else{
-		var timerNum = 30;
+		var timerNum = 30;	//默认30秒
 	}
+	//预存按钮初始文字，设置禁用状态
 	$(dom).prop('disabled',true);
 	$(dom).data('text',$(dom).text());
+
+	//开始倒计时
 	var timer;
 	timer = window.setInterval(function(){
 		timerNum --;
